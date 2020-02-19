e45d7879e161fa9dcb7342a4df376eaf158409dd
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2020-02-20 02:58:44 +0530

    Categories Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  
+  resources :categories
+  
   get 'welcome/index'
   get 'home/index'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,8 @@
 
 Rails.logger.info '1. Creating users'
 
+User.destroy_all
+
 10.times do
   User.create!(
     name: Faker::Name.name,
@@ -20,3 +22,46 @@ Rails.logger.info '1. Creating users'
     longitude: Faker::Address.longitude
   )
 end
+
+##############################################################################
+
+Rails.logger.info '2. Creating Categories'
+
+Category.destroy_all
+
+categories = Category.create([
+                               { name: 'Travel' },
+                               { name: 'Shopping' },
+                               { name: 'Bills' },
+                               { name: 'Food' },
+                               { name: 'Events' },
+                               { name: 'Others' }
+                             ])
+Category.create([
+                  { name: 'Bus', parent_id: categories.first.id },
+                  { name: 'Flights', parent_id: categories.first.id },
+                  { name: 'Cab', parent_id: categories.first.id },
+                  { name: 'Hotels', parent_id: categories.first.id }
+                ])
+Category.create([
+                  { name: 'Health', parent_id: categories.second.id },
+                  { name: 'Fashion', parent_id: categories.second.id },
+                  { name: 'Furniture', parent_id: categories.second.id },
+                  { name: 'Appliances', parent_id: categories.second.id },
+                  { name: 'EyeCare', parent_id: categories.second.id }
+                ])
+Category.create([
+                  { name: 'PostPaid', parent_id: categories.third.id },
+                  { name: 'Prepaid', parent_id: categories.third.id },
+                  { name: 'DTH', parent_id: categories.third.id },
+                  { name: 'LandLine', parent_id: categories.third.id },
+                  { name: 'Electricity', parent_id: categories.third.id },
+                  { name: 'Broadband', parent_id: categories.third.id }
+                ])
+Category.create([
+                  { name: 'Delivery', parent_id: categories.fourth.id },
+                  { name: 'Restaurants', parent_id: categories.fourth.id }
+                ])
+Category.create([{ name: 'Movies', parent_id: categories.fifth.id },
+                 { name: 'Games', parent_id: categories.fifth.id },
+                 { name: 'Events', parent_id: categories.fifth.id }])
